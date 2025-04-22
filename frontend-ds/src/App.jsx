3878785cddaec5c295b11dfe055e0480cf2128fb
Sasun Priyanath <158008110+Priyanath24460@users.noreply.github.com>
2025-04-22 14:35:10 +0530

    dsd

--- frontend-ds/src/App.jsx
+++ frontend-ds/src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import DriverRegisterForm from './driver/DriverRegisterForm'
-import DriverLoginForm from './driver/DriverLoginForm'
-import DriverDashboard from './driver/DriverDashboard'
-import TrackingMap from './components/TrackingMap'
+
+
+
+
+
 
 import CustomerLoginForm from './customer/CustomerLoginForm'
 import CustomerRegisterForm from './customer/CustomerRegisterForm'
 
-import DriverProfile from './driver/DriverProfile'
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DriverRegisterForm from './driver/DriverRegisterForm';
 import DriverLoginForm from './driver/DriverLoginForm';
